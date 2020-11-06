a9fb939b28c45eda8624279b6822c491b80027a0
norman <norman.brass@de.ibm.com>
2020-11-06 10:43:34 +0100

    Changed setting of hostname and port via value

--- complete/src/main/java/com/example/consumingrest/ShoutController.java
+++ complete/src/main/java/com/example/consumingrest/ShoutController.java
@@ -1,18 +1,32 @@
 package com.example.consumingrest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
+/**
+ * @author nbrass
+ *
+ * Start application for local testing with following command:
+ * 		./mvnw -Dspring-boot.run.arguments=--greetings.hostname=localhost spring-boot:run
+ */
 @RestController
 public class ShoutController {
+
+	@Value("${greetings.hostname:greetings}")
+	private String greetingsHostname;
+	
+	@Value("${greetings.port:8080}")
+	private Integer greetingsPort;
 
 	@GetMapping("/shout")
 	public Greeting shout(@RequestParam(value = "name", defaultValue = "World") String name) {
 		RestTemplate restTemplate = new RestTemplate();
 		HelloResponse helloResp = restTemplate.getForObject(
-				"http://localhost:8080/greeting?name="+name, HelloResponse.class);
+				"http://"+ greetingsHostname +":"+ greetingsPort + "/greeting?name="+name, HelloResponse.class);
 		return new Greeting(helloResp.getId(), helloResp.getContent().toUpperCase());
 	}
 }
